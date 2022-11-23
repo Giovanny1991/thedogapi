b2aac62f8840240cb03ea1e6d0ae3e7bba8ededc
Giovanny1991 <alexge2009@hotmail.com>
2022-11-22 20:09:56 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+// import './App.css';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://api.thedogapi.com/v1/images/search')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="posts-container">
+      {posts.map((post) => {
+        return (
+
+          <div>
+            <b >ID Imagen:</b>
+            {post.id}
+            <br/>
+              <img  src={post.url} width='500' alt='Cargando imagen API'/>
+          </div>
+        );
+      })}
     </div>
+
   );
-}
+};
 
 export default App;
